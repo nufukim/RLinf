181438f374f6e877a4c310935899eb718374d19b
nufukim <3098340041@qq.com>
2026-01-30 12:36:44 +0800

    feat: update rStar2 ci
    Signed-off-by: nufukim <3098340041@qq.com>

--- rlinf/workers/actor/megatron_actor_worker.py
+++ rlinf/workers/actor/megatron_actor_worker.py
@@ -874,7 +874,7 @@ class MegatronActor(MegatronModelManager, Worker):
         ):
             batch = self._dp_load_balance(batch)
 
-        if not batch.data:
+        if not batch:
             return None, None
 
         # Advantage normalization
